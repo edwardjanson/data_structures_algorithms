1d0808ea199f98e5615c70ddb892f489cb21748e
Edward Janson <99486978+edwardjanson@users.noreply.github.com>
2023-02-27 09:25:52 +0000

    added removefirst method to linkedlist class

--- src/main/java/linkedlist/LinkedList.java
+++ src/main/java/linkedlist/LinkedList.java
@@ -89,4 +89,14 @@ public class LinkedList {
         }
         length++;
     }
+
+    public Node removeFirst() {
+        if (this.length == 0) return null;
+        Node temp = this.head;
+        this.head = this.head.next;
+        temp.next = null;
+        length--;
+        if (length == 0) this.tail = null;
+        return temp;
+    }
 }
--- src/main/java/linkedlist/Main.java
+++ src/main/java/linkedlist/Main.java
@@ -14,15 +14,15 @@ public class Main {
         LinkedList myLinkedList = new LinkedList(4);
         myLinkedList.append(2);
         myLinkedList.append(3);
-
         getLinkedListInfo(myLinkedList);
 
         myLinkedList.removeLast();
-
         getLinkedListInfo(myLinkedList);
 
         myLinkedList.prepend(6);
+        getLinkedListInfo(myLinkedList);
 
+        myLinkedList.removeFirst();
         getLinkedListInfo(myLinkedList);
 
         System.out.println("\nLinked List:");
