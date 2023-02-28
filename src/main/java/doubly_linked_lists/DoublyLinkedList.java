63c6293f01a0358a0b3b64d9bd27af4359821cfb
Edward Janson <99486978+edwardjanson@users.noreply.github.com>
2023-02-28 10:31:26 +0000

    added append method to doublylinkedlist class

--- src/main/java/doubly_linked_lists/DoublyLinkedList.java
+++ src/main/java/doubly_linked_lists/DoublyLinkedList.java
@@ -50,4 +50,17 @@ public class DoublyLinkedList {
     public void getLength() {
         System.out.println("Length: " + length);
     }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        length++;
+    }
 }
--- src/main/java/doubly_linked_lists/Main.java
+++ src/main/java/doubly_linked_lists/Main.java
@@ -1,14 +1,23 @@
 package doubly_linked_lists;
 
+import linked_list.LinkedList;
+
 public class Main {
+
+    public static void getDoublyLinkedListInfo(DoublyLinkedList doublyLinkedList) {
+        doublyLinkedList.getHead();
+        doublyLinkedList.getTail();
+        doublyLinkedList.getLength();
+        System.out.println();
+    }
 
     public static void main(String[] args) {
 
         DoublyLinkedList myDLL = new DoublyLinkedList(7);
+        getDoublyLinkedListInfo(myDLL);
 
-        myDLL.getHead();
-        myDLL.getTail();
-        myDLL.getLength();
+        myDLL.append(2);
+        getDoublyLinkedListInfo(myDLL);
 
         System.out.println("\nDoubly Linked List:");
         myDLL.printList();
