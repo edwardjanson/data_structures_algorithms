d76dcc3e86a3dcb68a2234e8fdb8b40718ef630b
Edward Janson <99486978+edwardjanson@users.noreply.github.com>
2023-02-28 13:45:29 +0000

    added queue class and constructor

---
+++ src/main/java/Queue/Main.java
@@ -0,0 +1,16 @@
+package Queue;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Queue myQueue = new Queue(4);
+
+        myQueue.getFirst();
+        myQueue.getLast();
+        myQueue.getLength();
+
+        System.out.println("\nQueue:");
+        myQueue.printQueue();
+    }
+}
--- src/main/java/stack/Stack.java
+++ src/main/java/stack/Stack.java
@@ -58,4 +58,5 @@ public class Stack {
         this.height--;
         return temp;
     }
+
 }
