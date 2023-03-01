61d694f2afd862d32d5b9cf6b896e095b53bcfbc
Edward Janson <99486978+edwardjanson@users.noreply.github.com>
2023-03-01 15:11:20 +0000

    added quick sort methods

--- src/main/java/sorts/Main.java
+++ src/main/java/sorts/Main.java
@@ -20,11 +20,20 @@ public class Main {
 
         int[] array1 = {2, 6, 9, 10, 12};
         int[] array2 = {4, 5, 8, 11, 15};
-        int[] arraySorted = Sort.merge(array1, array2);
+        int[] arraySorted = MergeSort.merge(array1, array2);
         System.out.println(Arrays.toString(arraySorted));
 
         int[] mergeSortArray = {3, 8, 4, 12, 9};
-        int[] arraySorted2 = Sort.mergeSort(mergeSortArray);
+        int[] arraySorted2 = MergeSort.mergeSort(mergeSortArray);
         System.out.println(Arrays.toString(arraySorted2));
+
+        int[] array3 = {4, 6, 1, 7, 3, 2, 5};
+        int returnedIndex = QuickSort.pivot(array3, 0, 6);
+        System.out.println("Returned Index: " + returnedIndex);
+        System.out.println(Arrays.toString(array3));
+
+        int[] array4 = {4, 6, 1, 7, 3, 2, 5};
+        QuickSort.quickSort(array4);
+        System.out.println(Arrays.toString(array4));
     }
 }
