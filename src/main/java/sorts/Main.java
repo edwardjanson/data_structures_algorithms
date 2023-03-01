f481d3335f932c11f34440ae0bd9097cd9fbde80
Edward Janson <99486978+edwardjanson@users.noreply.github.com>
2023-03-01 13:40:18 +0000

    added merge method

--- src/main/java/sorts/Main.java
+++ src/main/java/sorts/Main.java
@@ -1,4 +1,4 @@
-package basic_sorts;
+package sorts;
 
 import java.util.Arrays;
 
@@ -17,5 +17,10 @@ public class Main {
         int[] insertionArray = {2, 6, 9, 12, 7};
         Sort.insertionSort(insertionArray);
         System.out.println(Arrays.toString(insertionArray));
+
+        int[] array1 = {2, 6, 9, 10, 12};
+        int[] array2 = {4, 5, 8, 11, 15};
+        int[] arraySorted = Sort.merge(array1, array2);
+        System.out.println(Arrays.toString(arraySorted));
     }
 }
